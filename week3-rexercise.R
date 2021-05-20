7ced1725687275e8888ce0cf45a3ea8161cf42dc
Kristian Krankenbuhl <chkr0001@students.zhaw.ch>
2021-05-20 22:06:53 +0200

    created 6 new pedestrian matrices

--- week3-rexercise.R
+++ week3-rexercise.R
@@ -85,3 +85,11 @@ pedestrian %>%
   facet_wrap(~TrajID,nrow=2) 
 
 #Task 6
+library(SimilarityMeasures)
+#Creating a matrix for each pedestrian
+pedestrian1 <- as.matrix(filter(pedestrian[,2:3], pedestrian$TrajID == "1"))
+pedestrian2 <- as.matrix(filter(pedestrian[,2:3], pedestrian$TrajID == "2"))
+pedestrian3 <- as.matrix(filter(pedestrian[,2:3], pedestrian$TrajID == "3"))
+pedestrian4 <- as.matrix(filter(pedestrian[,2:3], pedestrian$TrajID == "4"))
+pedestrian5 <- as.matrix(filter(pedestrian[,2:3], pedestrian$TrajID == "5"))
+pedestrian6 <- as.matrix(filter(pedestrian[,2:3], pedestrian$TrajID == "6"))
